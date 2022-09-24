a65652764957e4ef48d354f7676e567ae38222d4
Rebe <rlhenshaw@yahoo.com>
2022-09-24 14:31:40 -0700

    created a navigation menu

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -10,20 +10,28 @@ class Dictionary:
         wp = jp.QuasarPage(tailwind=True)
         div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
         jp.Div(a=div, text="Instant English Dictionary", classes="text-4xl m-2")
-        jp.Div(a=div, text="Get the definition of any English word instantly as you type.", classes="text-lg")
-
-        input_div = jp.Div(a=div, classes="grid grid-cols-2")
-        input_box = jp.Input(a=input_div, placeholder="Type your word here...",
-                             classes="m-2 bg-gray-100 border-2 border-gray-200 rounded "
-                                     "width-64 focus:bg-white focus:outline-none focus:border-purple-500 py-2 px-4")
+        jp.Div(a=div, text="Get the definition of any English word instantly as you type.", classes="text-lg m-2")
 
         output_div = jp.Div(a=div, classes="text-lg m-2 p-2 border-2 h-40")
-        jp.Button(a=input_div, text="Get Definition", classes="border-2 text-gray-500",
-                  click=cls.get_definition, outputdiv=output_div, inputbox=input_box)
+        input_div = jp.Div(a=div, classes="grid grid-cols-2")
+        input_box = jp.Input(a=input_div, placeholder="Type your word here...", outputdiv=output_div,
+                             classes="m-2 bg-gray-100 border-2 border-gray-200 rounded "
+                                     "width-64 focus:bg-white focus:outline-none focus:border-purple-500 py-2 px-4")
+        input_box.on('input', cls.get_definition)
+
+
+        # jp.Button(a=input_div, text="Get Definition", classes="border-2 text-gray-500",
+        #           click=cls.get_definition, outputdiv=output_div, inputbox=input_box)
 
         return wp
 
     @staticmethod
     def get_definition(widget, msg):
-        defined = definition.Definition(widget.inputbox.value).get()
-        widget.outputdiv.text = defined
+        defined = definition.Definition(widget.value).get()
+        output_text = ""
+        counter = 1
+        for each in defined:
+            output_text += f"\n{str(counter)}. {each}"
+            counter += 1
+
+        widget.outputdiv.text = output_text
--- webapp/home.py
+++ webapp/home.py
@@ -3,14 +3,43 @@ import justpy as jp
 class Home:
     path = "/"
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
-        jp.Div(a=div, text="Hello World!! This is home", classes="text-4xl m-2")
+
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+        tool = jp.QToolbar(a=header)
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_mode="left", bordered=True)
+
+        scroll = jp.QScrollArea(a=drawer, classes="fit")
+        list_area = jp.QList(a=scroll)
+        a_classes = "p-2 m-2 text-lg text-blue-400 hover:text-blue-700"
+        jp.A(a=list_area, text="Home", href="/", classes=a_classes)
+        jp.Br(a=list_area)
+        jp.A(a=list_area, text="About", href="/about", classes=a_classes)
+        jp.Br(a=list_area)
+        jp.A(a=list_area, text="Dictionary", href="/dictionary", classes=a_classes)
+
+        jp.QBtn(a=tool, dense=True, flat=True, round=True, icon="menu",
+                click=cls.move_drawer, drawer=drawer)
+        jp.QToolbarTitle(a=tool, text="Instant Dictionary")
+        container = jp.QPageContainer(a=layout)
+        div = jp.Div(a=container, classes="bg-gray-200 h-screen")
+        jp.Div(a=div, text="Hello World!! This is home", classes="text-4xl m-2 p-4")
         jp.Div(a=div, text="""
                 Latin Text Meow
-                """, classes="text-lg")
+                """, classes="text-lg p-4")
         return wp
+
+
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
+
 
 
 
